f9b55f60c9408e0039a736e33f9213a8d0d84d15
Ennyola <medunoyeeni@gmail.com>
2020-08-26 22:07:12 +0100

    Added a change status functionality

--- admin_app/admin.py
+++ admin_app/admin.py
@@ -9,20 +9,22 @@ from django.core.mail import send_mail
 admin.site.site_header = "Django User Administration"
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'date_joined', 'email', 'set_active_buttons', 'is_active']
+    list_display = ['username', 'date_joined', 'email', 'change_active_status', 'is_active', 'change_staff_status', 'is_staff']
     list_display_links = ['username', 'email']
     list_filter = ['date_joined']
-    # search_fields = ['username', 'email']
+    search_fields = ['username', 'email']
     change_list_template = "admin/admin_app/change_list.html"
 
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
             path('is_active/<user>', self.change_active),
-            path ('send_mails/', self.send_mails )
+            path ('send_mails/', self.send_mails ),
+            path('is_staff/<user>', self.is_staff_status)
         ]
         return custom_urls + urls
 
+    # Views for the change_active status functionality
     def change_active(self, request, user):
         user = User.objects.get(username = user)
         if user.is_active:
@@ -35,21 +37,38 @@ class UserAdmin(admin.ModelAdmin):
             self.message_user(request, 'User is now active')
         return redirect('../')
 
+    # Views for the change_staff_status functionality
+    def is_staff_status(self, request, user):
+        user = User.objects.get(username = user)
+        if user.is_staff:
+            user.is_staff  = False
+            user.save()
+            self.message_user(request, 'User now has staff status')
+        else:
+            user.is_staff = True
+            user.save()
+            self.message_user(request, 'User no longer has staff status')
+        return redirect('../')
+
+    # Views handling the sending of mails
     def send_mails(self, request):
         subject = request.POST.get("mail-subject", None)
         message = request.POST.get("mail-body", None)
+        from_email = request.user.email
         users = User.objects.all()
-        mails = [user.email for user in users]
-        send_mail(subject, message, 'medunoyeeni@gmail.com', mails, fail_silently=True)
-        
-
-
+        recipient_list = [user.email for user in users]
+        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
         self.message_user(request, "Mails Sent successfully")
         return redirect('../')     
 
-    def set_active_buttons(self, obj):
+    def change_active_status(self, obj):
         return format_html(
-            f'<a class = "button" href ="is_active/{obj.username}"> Change Active </a>'
+            f'<a class = "button" href ="is_active/{obj.username}"> Change Active status</a>'
+        )
+
+    def change_staff_status(self, obj):
+        return format_html(
+            f'<a class = "button" href ="is_staff/{obj.username}"> Change Staff status </a>'
         )
    
 
--- custom_admin_interface/settings.py
+++ custom_admin_interface/settings.py
@@ -119,10 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "medunoyeeni@gmail.com"
-EMAIL_HOST_PASSWORD = 'Enieemed'
-EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
